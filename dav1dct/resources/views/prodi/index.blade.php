2982eff2f33445c34357ebc2595c2a9f5d3649d5
dav1dct <lindahwatih215@gmail.com>
2024-05-21 16:36:32 +0700

    Update 0.14.9.29997

--- dav1dct/resources/views/prodi/index.blade.php
+++ dav1dct/resources/views/prodi/index.blade.php
@@ -1,20 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">3
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Program Studi</title>
-</head>
-<body>
-    <h1>UMDP</h1>
-    <h2>Program Studi</h2>
 
-    <ul>
-        @foreach ($prodi as $item)
+        {{-- @foreach ($prodi as $item)
             <li>{{ $item['nama'] }} {{ $item['singkatan'] }}
-            {{ $item["fakultas"]["nama"] }}</li>
-        @endforeach
-    </ul>
-</body>
-</html>
+            {{ $item["fakultas"]["nama"] }}</li> --}}
+
+@extends('layout.main')
+
+@section('title','Fakultas')
+
+@section('content')
+    <div class="col-lg-6 grid-margin stretch-card">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title">Prodi</h4>
+            <p class="card-description">
+              List data Prodi
+            </p>
+            <div class="table-responsive">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>Nama Prodi</th>
+                    <th>Singkatan</th>
+                    <th>Fakultas</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    @foreach ($prodi as $item)
+                    <tr>
+                        <td>{{ $item['nama'] }}</td>
+                        <td>{{ $item['singkatan'] }}</td>
+                        <td>{{ $item["fakultas"]["nama"] }}</td>
+                    </tr>
+                @endforeach  
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+@endsection
